e94178c87bb240dde16d759d6c5c83e2506ac3ce
Alex Hardman <alexhardman@launchdarkly.com>
2022-10-05 13:57:47 -0500

    adding device data

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import * as React from 'react';
 import IconButton from '@mui/material/IconButton';
 import Box from '@mui/material/Box';
 import AppBar from '@mui/material/AppBar';
-import Avatar from '@mui/material/Avatar';
 import Typography from '@mui/material/Typography';
 import Toolbar from '@mui/material/Toolbar';
 import {useTheme, ThemeProvider, createTheme} from '@mui/material/styles';
@@ -11,15 +10,10 @@ import Brightness7Icon from '@mui/icons-material/Brightness7';
 import CssBaseline from '@mui/material/CssBaseline';
 import Picker from '@emoji-mart/react';
 import axios from 'axios';
-import Filter1Icon from '@mui/icons-material/Filter1';
-import Filter2Icon from '@mui/icons-material/Filter2';
-import Filter3Icon from '@mui/icons-material/Filter3';
-import Filter4Icon from '@mui/icons-material/Filter4';
-import Filter5Icon from '@mui/icons-material/Filter5';
-import Filter6Icon from '@mui/icons-material/Filter6';
-import Filter7Icon from '@mui/icons-material/Filter7';
-import Filter8Icon from '@mui/icons-material/Filter8';
-import Filter9Icon from '@mui/icons-material/Filter9';
+import {useFlags, useLDClient} from 'launchdarkly-react-client-sdk';
+import PropTypes from 'prop-types';
+import stringHash from 'string-hash';
+import {useDeviceData} from 'react-device-detect';
 
 import {
 	Card,
@@ -30,36 +24,41 @@ import {
 	Grid,
 	Popover,
 	Button,
-	ToggleButtonGroup,
-	ToggleButton,
 	Tooltip,
+	TextField,
 } from '@mui/material';
 
 const ColorModeContext = React.createContext({toggleColorMode() {}});
 
-function Toggler() {
+const randomNumberGenerator = () => {
+	return Math.floor(1_000_000_000 + Math.random() * 9_000_000_000);
+};
+
+function Toggler({login, userKey}) {
 	const theme = useTheme();
+
+	const [temporaryPhoneNumber, setTemporaryPhoneNumber] = React.useState(
+		`+1${randomNumberGenerator()}1`,
+	);
+	const [isChangingPhoneNumber, setIsChangingPhoneNumber] =
+		React.useState('false');
+
 	const colorMode = React.useContext(ColorModeContext);
+
+	const {darkMode} = useFlags();
+
+	const phoneNumberChange = (event) => {
+		setTemporaryPhoneNumber(event.target.value);
+	};
+
 	return (
 		<AppBar
 			position="sticky"
 			color="inherit"
-			sx={{top: 'auto', bottom: 0, width: '100%'}}
+			sx={{top: 'auto', bottom: 0, width: '100%', alignItems: 'center'}}
 		>
 			<Toolbar color="inherit">
-				<Box
-					sx={{
-						display: 'flex',
-						width: '100%',
-						alignItems: 'center',
-						justifyContent: 'center',
-						bgcolor: 'background.default',
-						color: 'text.primary',
-						borderRadius: 1,
-						p: 3,
-					}}
-				>
-					{theme.palette.mode} mode
+				{darkMode ? (
 					<IconButton
 						sx={{ml: 1}}
 						color="inherit"
@@ -71,13 +70,55 @@ function Toggler() {
 							<Brightness4Icon />
 						)}
 					</IconButton>
-				</Box>
+				) : null}
+				{!isChangingPhoneNumber && userKey ? (
+					<>
+						<Typography>{userKey}</Typography>
+						<Button
+							variant="contained"
+							color="secondary"
+							sx={{margin: 2}}
+							onClick={() => {
+								setIsChangingPhoneNumber(true);
+							}}
+						>
+							Change User Key
+						</Button>
+					</>
+				) : (
+					<>
+						<TextField
+							sx={{marginTop: 2}}
+							id="phoneNumber"
+							label="Telephone Number"
+							value={temporaryPhoneNumber}
+							helperText="Starts as randomly assigned number"
+							onChange={phoneNumberChange}
+						/>
+						<Button
+							variant="contained"
+							color="secondary"
+							sx={{margin: 2}}
+							onClick={() => {
+								login(temporaryPhoneNumber);
+								setIsChangingPhoneNumber(false);
+							}}
+						>
+							Save
+						</Button>
+					</>
+				)}
 			</Toolbar>
 		</AppBar>
 	);
 }
 
-function MyApp() {
+Toggler.propTypes = {
+	login: PropTypes.func,
+	userKey: PropTypes.string,
+};
+
+function MyApp({login, userKey}) {
 	const [emoji, setEmoji] = React.useState();
 	const [pickerOpen, setPickerOpen] = React.useState(false);
 	const [emojiTitle, setEmojiTitle] = React.useState();
@@ -88,13 +129,12 @@ function MyApp() {
 		async function fetchData() {
 			try {
 				if (emoji) {
-					console.log('segment id', segmentId);
 					const response = await axios({
 						baseURL: import.meta.env.VITE_API_URL,
 						url: import.meta.env.VITE_API_PATH,
 						params: {
 							emoji,
-							segmentId,
+							userKey,
 						},
 					});
 
@@ -112,7 +152,6 @@ function MyApp() {
 	}, [emoji]);
 
 	const [anchorElement, setAnchorElement] = React.useState();
-
 	const open = Boolean(anchorElement);
 	const id = open ? 'emoji=picker' : undefined;
 
@@ -126,20 +165,14 @@ function MyApp() {
 		setAnchorElement(null);
 	};
 
-	const [segmentId, setSegmentId] = React.useState(0);
-
-	const handleSegmentChange = (event, newSegmentId) => {
-		setSegmentId(newSegmentId);
-	};
-
 	return (
 		<>
-			<Toggler />
+			<Toggler login={login} userKey={userKey} />
 			<Popover
 				open={pickerOpen}
 				id={id}
 				anchorOrigin={{
-					vertical: 'bottom',
+					vertical: 'top',
 					horizontal: 'left',
 				}}
 				anchorEl={anchorElement}
@@ -162,43 +195,6 @@ function MyApp() {
 			>
 				<Grid item xs={0} md={4} lg={4} />
 				<Grid item xs={12} md={4} lg={4}>
-					<Typography gutterBottom variant="h4" component="div">
-						Please select a value to begin
-					</Typography>
-					<ToggleButtonGroup
-						exclusive
-						value={segmentId}
-						aria-label="text alignment"
-						onChange={handleSegmentChange}
-					>
-						<ToggleButton value={1}>
-							<Filter1Icon />
-						</ToggleButton>
-						<ToggleButton value={2}>
-							<Filter2Icon />
-						</ToggleButton>
-						<ToggleButton value={3}>
-							<Filter3Icon />
-						</ToggleButton>
-						<ToggleButton value={4}>
-							<Filter4Icon />
-						</ToggleButton>
-						<ToggleButton value={5}>
-							<Filter5Icon />
-						</ToggleButton>
-						<ToggleButton value={6}>
-							<Filter6Icon />
-						</ToggleButton>
-						<ToggleButton value={7}>
-							<Filter7Icon />
-						</ToggleButton>
-						<ToggleButton value={8}>
-							<Filter8Icon />
-						</ToggleButton>
-						<ToggleButton value={9}>
-							<Filter9Icon />
-						</ToggleButton>
-					</ToggleButtonGroup>
 					<Card
 						disabled
 						sx={{
@@ -206,8 +202,10 @@ function MyApp() {
 						}}
 					>
 						<CardHeader
-							avatar={<Avatar>9K</Avatar>}
 							title="EmojiSplainerBot 9000"
+							sx={{
+								textAlign: 'center',
+							}}
 						/>
 						<CardContent>
 							<CardMedia
@@ -217,23 +215,21 @@ function MyApp() {
 								alt="emojisplainer bot 9000"
 							/>
 							<CardContent>
-								<Typography gutterBottom variant="h5" component="div">
-									EmojiSplainerBot 9000
-								</Typography>
-								<Typography variant="body2" color="text.secondary">
-									Tell me your emoji and I will explain it to you!
+								<Typography
+									gutterBottom
+									variant="h5"
+									component="div"
+									sx={{textAlign: 'center'}}
+								>
+									Pick an your emoji and I will explain it to you!
 								</Typography>
 							</CardContent>
 						</CardContent>
-						<CardActions>
+						<CardActions sx={{justifyContent: 'center'}}>
 							<Tooltip title="please select a number at the top first">
 								<span>
-									<Button
-										disabled={segmentId === 0}
-										variant="contained"
-										onClick={openPicker}
-									>
-										Open Picker
+									<Button variant="contained" onClick={openPicker}>
+										Open Emoji Picker
 									</Button>
 								</span>
 							</Tooltip>
@@ -242,13 +238,33 @@ function MyApp() {
 					<br />
 					{emoji ? (
 						<Card>
-							<CardHeader title={emojiTitle} />
-							<CardMedia>
-								<Typography gutterBottom variant="h1" component="div">
-									{emoji}
-								</Typography>
-							</CardMedia>
-							<CardContent>{emojiUsage}</CardContent>
+							<Box sx={{alignItems: 'center'}}>
+								<Box sx={{alignItems: 'center'}}>
+									<CardMedia>
+										<Typography
+											gutterBottom
+											variant="h2"
+											component="div"
+											sx={{textAlign: 'center'}}
+										>
+											{emoji}
+										</Typography>
+									</CardMedia>
+								</Box>
+								<CardContent>
+									<Typography
+										gutterBottom
+										variant="h5"
+										component="div"
+										sx={{textAlign: 'center'}}
+									>
+										{emojiTitle}
+									</Typography>
+									<Typography variant="body2" color="text.secondary">
+										{emojiUsage}
+									</Typography>
+								</CardContent>
+							</Box>
 						</Card>
 					) : null}
 				</Grid>
@@ -257,8 +273,40 @@ function MyApp() {
 	);
 }
 
+MyApp.propTypes = {
+	login: PropTypes.func,
+	userKey: PropTypes.string,
+};
+
 export default function ToggleColorMode() {
 	const [mode, setMode] = React.useState('light');
+
+	const launchDarklyClient = useLDClient();
+	const [userKey, setUserKey] = React.useState();
+	const [phoneNumber, setPhoneNumber] = React.useState();
+	const deviceData = useDeviceData();
+
+	console.log(deviceData);
+
+	// Const [item, setItem] = useLocalStorage('name', 'Initial Value');
+
+	React.useEffect(() => {
+		if (phoneNumber) {
+			setUserKey(`${stringHash(phoneNumber)}${phoneNumber.slice(-1)}`);
+		}
+
+		if (userKey) {
+			launchDarklyClient.identify({
+				key: userKey,
+				custom: {
+					browserName: deviceData.browser.Name,
+					deviceName: deviceData.model,
+					osName: deviceData.os.name,
+				},
+			});
+		}
+	}, [userKey, phoneNumber, launchDarklyClient, deviceData]);
+
 	const colorMode = React.useMemo(
 		() => ({
 			toggleColorMode() {
@@ -284,7 +332,7 @@ export default function ToggleColorMode() {
 		<ColorModeContext.Provider value={colorMode}>
 			<ThemeProvider theme={theme}>
 				<CssBaseline />
-				<MyApp />
+				<MyApp login={setPhoneNumber} userKey={userKey} />
 			</ThemeProvider>
 		</ColorModeContext.Provider>
 	);
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 
 import ReactDOM from 'react-dom/client';
+import {withLDProvider} from 'launchdarkly-react-client-sdk';
 import App from './App';
+
+const LDProvider = withLDProvider({
+	clientSideID: import.meta.env.VITE_CLIENT_ID,
+	options: {
+		/* ... */
+	},
+})(App);
 
 // eslint-disable-next-line no-undef
 ReactDOM.createRoot(document.querySelector('#root')).render(
 	<React.StrictMode>
-		<App />
+		<LDProvider />
 	</React.StrictMode>,
 );
